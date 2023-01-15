c90b9a8c54340e6e509d68890e50af4f86a38daa
Angel Duarte <angel.dtd.esc@gmail.com>
2023-01-15 17:08:20 -0600

    mapping tables as entities

--- src/main/java/com/dev/apimarket/ApiMarketApplication.java
+++ src/main/java/com/dev/apimarket/ApiMarketApplication.java
@@ -1,9 +1,12 @@
 package com.dev.apimarket;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ApiMarketApplication {
 
 	public static void main(String[] args) {
